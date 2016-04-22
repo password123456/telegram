4bbefa1a7468c6c8e0eb71b7893e1165fbf9efc2
password123456 <sendmefuture@gmail.com>
2016-04-22 23:23:07 +0900

    Create sendmessage.py

---
+++ sendmessage.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python2.7
+# -*- coding: utf-8 -*-
+__author__ = https://github.com/password123456/
+
+import urllib
+import urllib2
+
+bot_id = 'Your Bot ID'
+url = 'https://api.telegram.org/bot' + bot_id + '/sendMessage'
+
+data = {}
+data['chat_id'] = 'Your chat room number'
+data['text'] = 'hello buddy'
+
+request  = urllib2.Request(url, urllib.urlencode(data))
+response = urllib2.urlopen(request)
+res_data = response.read()
+
+print res_data
